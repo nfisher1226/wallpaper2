270ba0047dbe426de9f7b0d437b514b04b269b53
Nathan Fisher <nfisher.sr@gmail.com>
2013-08-07 06:57:30 -0400

    Fixed handling of spaces in filenames. Fixed error messages in terminal while typing directory path

--- lib/buildlist.sh
+++ lib/buildlist.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-ls -1 "$(cat $HOME/.config/wallpaper/wp-dir)" \
-  | egrep '.png|.jpg|.jpeg|.svg|.JPG|.PNG' | \
-while read IMG
-do
- echo "$IMG"
+WP_DIR="$(cat $HOME/.config/wallpaper/wp-dir)"
+[ -d "$WP_DIR" ] && ls -1 "$(cat $HOME/.config/wallpaper/wp-dir)" \
+  | egrep '.png|.jpg|.jpeg|.svg|.JPG|.PNG' | while read IMG
+  do echo "$IMG"
 done
