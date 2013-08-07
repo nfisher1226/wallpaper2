cbdfb33f52507be715d428f25308ea03f11b74ef
Nathan Fisher <nfisher.sr@gmail.com>
2013-08-07 16:22:19 -0400

    Create and use single state file in configdir

--- lib/buildlist.sh
+++ lib/buildlist.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-WP_DIR="$(cat $HOME/.config/wallpaper/wp-dir)"
-[ -d "$WP_DIR" ] && ls -1 "$(cat $HOME/.config/wallpaper/wp-dir)" \
+[ -d "$WP_DIR" ] && ls -1 "$WP_DIR" \
   | egrep '.png|.jpg|.jpeg|.svg|.JPG|.PNG' | while read IMG
   do echo "$IMG"
 done
--- lib/set-wallpaper.sh
+++ lib/set-wallpaper.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-WP_BACKEND="$(cat $USERCONFIGDIR/backend)"
-[ "$WP_BACKEND" = "" ] && WP_BACKEND=feh
-. $LIBDIR/plugins/${WP_BACKEND}.sh
-set_wallpaper "$1" "$2"
+[ "$BACKEND" = "" ] && BACKEND=feh
+. $LIBDIR/plugins/${BACKEND}.sh
+set_wallpaper "$MODE" "$WP_DIR/$WP"
