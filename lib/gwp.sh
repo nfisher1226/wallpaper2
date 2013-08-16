f5c843c74adf7c1497f410db4f418d9da91736bc
Nathan Fisher <nfisher.sr@gmail.com>
2013-08-15 23:44:31 -0400

    Created slideshow editor Fixed corruption of statefile on WM close Revamped list creation and display Moved most preferences to menubar Tweaked gui rendering
    The slideshow editor can currently add and remove files, create
    new slideshows, and change between different slideshows. There
    is no playback feature yet.

--- lib/gwp.sh
+++ lib/gwp.sh
@@ -1,7 +1,14 @@
 set_wallpaper () {
-[ -f "$USERCONFIGDIR/conf" ] && . "$USERCONFIGDIR/conf"
+[ "$STRETCH" = "true" ] && MODE=Stretch
+[ "$SCALE" = "true" ] && MODE=Scale
+[ "$CENTER" = "true" ] && MODE=Center
+[ "$TILE" = "true" ] && MODE=Tile
+[ "$FEH" = "true" ] && BACKEND=feh
+[ "$ROX" = "true" ] && BACKEND=rox
+[ "$IMLIBSETROOT" = "true" ] && BACKEND=imlibsetroot
+[ "ESETROOT" = "true" ] && BACKEND=esetroot
 [[ -z "$BACKEND" ]] && BACKEND=feh
 . $LIBDIR/plugins/${BACKEND}.sh
-set_wallpaper "$MODE" "$WP_DIR/$WP"
+set_wallpaper "$MODE" "$WP"
 }
 export -f set_wallpaper
